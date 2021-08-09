61af7bbc1cd154e14d531745940ee3457ab59e34
BubbleNeumann <53640776+BubbleNeumann@users.noreply.github.com>
2021-08-09 10:37:46 +0400

    all available answers are displayed

--- bot.py
+++ bot.py
@@ -9,19 +9,19 @@ bot = telebot.TeleBot(config.TOKEN)
 def displayKeyboard(message):
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard = True)
-    ans1 = types.KeyboardButton(config.ANSWERS[0])
-    ans2 = types.KeyboardButton(config.ANSWERS[1])
-    ans3 = types.KeyboardButton(config.ANSWERS[2])
-
-    markup.add(ans1, ans2, ans3)
+    
+    # display available answers as buttons
+    for i in range(len(MOODLIST)):
+        markup.row(types.KeyboardButton(MOODLIST[i]))
 
     bot.send_message(message.chat.id, "How do you feel?", reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def handleUserInput(message):
-    if message.text == config.ANSWERS[0]:
-         bot.send_message(message.chat.id, "good")
-    else:
-         bot.send_message(message.chat.id, "also good")
+
+    log = open("log.txt", 'a')
+    log.write(str(len(MOODLIST) - MOODLIST.index(message.text) - 1) + '|')
+
+    bot.send_message(message.chat.id, "Got it!")
 
 bot.polling(none_stop=True)
